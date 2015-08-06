bf2f694b7fd6476c2ca454dcc026a118c6cc9fba
715sunny <715xiaoyanzi@gmail.com>
2015-08-06 15:30:34 -0400

    set phone num

--- Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/OrderMenuDB.java
+++ Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/OrderMenuDB.java
@@ -25,6 +25,7 @@ public class OrderMenuDB {
     public static final String IMAGE = "image";
     public static final String SHORTCUTSKEY = "shortcutsKey";
 
+
     private static final int DATABASE_VERSION = 2;
 
     public static final String DATABASE_CREATE_ORDERS = "CREATE TABLE "
@@ -97,10 +98,10 @@ public class OrderMenuDB {
 
     public void persist(String name, String image, String shortcuts, int type) throws SQLException {
         ContentValues values = new ContentValues();
-        MenusContract.putName(values, name);
-        MenusContract.putImage(values, image);
-        MenusContract.putShortcutKey(values, shortcuts);
-        MenusContract.putType(values,type);
+//        MenusContract.putName(values, name);
+//        MenusContract.putImage(values, image);
+//        MenusContract.putShortcutKey(values, shortcuts);
+//        MenusContract.putType(values,type);
         db.insert(DATABASE_MENUS_TABLE, null, values);
     }
 
