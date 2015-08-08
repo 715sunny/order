c93bf1ddda5c37ee77dfe3f9c2c6000f14bd025a
yubousa10 <boyuhw@gmail.com>
2015-08-07 21:45:36 -0400

    菜单同步

--- Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/Order.java
+++ Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/Order.java
@@ -4,6 +4,7 @@ import android.app.TabActivity;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TabHost;
 
 import java.util.ArrayList;
@@ -11,7 +12,8 @@ import java.util.ArrayList;
 public class Order extends TabActivity
 
 {
-    private TabHost myTabHost;
+    public  static TabHost myTabHost;
+    public static ArrayList<String> array=new ArrayList<String>() ;
 
 
     @Override
@@ -22,13 +24,25 @@ public class Order extends TabActivity
         myTabHost=(TabHost)findViewById(android.R.id.tabhost);
         TabHost.TabSpec spec;
         Intent intent;
-        intent= new Intent(this,FastOrder.class);
+        intent = new Intent(this,FastOrder.class);
         spec= myTabHost.newTabSpec("FastOrder").setIndicator("快速点单").setContent(intent);
         myTabHost.addTab(spec);
-        intent= new Intent(this,allorder.class);
+        intent = new Intent(this,allorder.class);
         spec= myTabHost.newTabSpec("AllOrder").setIndicator("全部点单").setContent(intent);
         myTabHost.addTab(spec);
 
+        myTabHost.setOnTabChangedListener(new TabHost.OnTabChangeListener() {
+
+            @Override
+            public void onTabChanged(String tabId) {
+                if(allorder.listview!=null&&allorder.adapter!=null&&tabId.equals("AllOrder")){
+                    allorder.listview.setAdapter(allorder.adapter);
+                }else if(FastOrder.listview!=null&&FastOrder.adapter!=null&&tabId.equals("FastOrder")){
+                    FastOrder.listview.setAdapter(FastOrder.adapter);
+                }
+            }
+        });
     }
+
 
 }
