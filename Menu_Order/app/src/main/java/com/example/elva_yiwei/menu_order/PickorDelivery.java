075658190149cf087c4c5ef9261a645e22eeb9f6
715sunny <715xiaoyanzi@gmail.com>
2015-08-17 11:44:46 -0400

    已点菜品

--- Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/PickorDelivery.java
+++ Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/PickorDelivery.java
@@ -8,7 +8,6 @@ import android.widget.Button;
 
 
 public class PickorDelivery extends Activity {
-    private String cellphone = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,8 +15,6 @@ public class PickorDelivery extends Activity {
         setContentView(R.layout.activity_pickupordelivery);
         Button delivery_button = (Button) findViewById(R.id.delivery);
         Button pickup_button = (Button) findViewById(R.id.pickup);
-        Intent intent = getIntent();
-        cellphone = intent.getStringExtra("cellphone");
 
         pickup_button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,8 +22,9 @@ public class PickorDelivery extends Activity {
 
                 Intent t= new Intent();
                 t.setClass(PickorDelivery.this, Order.class);
-                t.putExtra("cellphone",cellphone);
                 t.putExtra("flag", 0);
+                Bundle extras = getIntent().getExtras();
+                String myText = extras.getString("myText");
                 startActivity(t);
             }
         });
@@ -36,7 +34,6 @@ public class PickorDelivery extends Activity {
             public void onClick(View v) {
                 Intent t= new Intent();
                 t.setClass(PickorDelivery.this, Order.class);
-                t.putExtra("cellphone",cellphone);
                 t.putExtra("flag", 1);
                 startActivity(t);
             }
