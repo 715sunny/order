31294a72ee6085d0b3743b346401c434f702b086
715sunny <715xiaoyanzi@gmail.com>
2015-08-28 12:34:46 -0400

    蓝牙模块

--- Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/bluetooth.java
+++ Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/bluetooth.java
@@ -1,22 +1,88 @@
 package com.example.elva_yiwei.menu_order;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
+import com.RT_Printer.BluetoothPrinter.BLUETOOTH.BluetoothPrintDriver;
+import android.view.View.OnClickListener;
+import android.widget.Toast;
 
 
 public class bluetooth extends ActionBarActivity {
 
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private BluetoothPrintDriver mChatService = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth);
+
+
+        Button bluetoothbutton=(Button)findViewById(R.id.bluetooth_link_button);
+
+
+        Button printbutton=(Button)findViewById(R.id.bluetooth_print_button);
+        bluetoothbutton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(BluetoothPrintDriver.IsNoConnection()){
+                    return;
+                }
+                BluetoothPrintDriver.Begin();
+
+                String tmpContent = "Sunny!!";
+                BluetoothPrintDriver.BT_Write(tmpContent);
+                BluetoothPrintDriver.BT_Write("\r");
+
+            }
+        });
+
+
+
     }
 
+    OnClickListener mBtnConnetBluetoothDeviceOnClickListener=new OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(bluetooth.this, DeviceListActivity.class);
+            startActivityForResult(intent, REQUEST_CONNECT_DEVICE);
+        }
+    };
 
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)  {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    // Get the device MAC address
+                    String address = data.getExtras()
+                            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                    // Get the BLuetoothDevice object
+                    BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+                    // Attempt to connect to the device
+                    mChatService.connect(device);
+                }
+                break;
+        }
+    }
+
+public void onClick_bt(View view){
+                Toast.makeText(getApplicationContext(),"testpress",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(bluetooth.this, DeviceListActivity.class);
+               startActivityForResult(intent, REQUEST_CONNECT_DEVICE);
+}
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/setting.java
+++ Menu_Order/app/src/main/java/com/example/elva_yiwei/menu_order/setting.java
@@ -1,27 +1,175 @@
 package com.example.elva_yiwei.menu_order;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
+
+import com.RT_Printer.BluetoothPrinter.BLUETOOTH.BluetoothPrintDriver;
 
 
 public class setting extends ActionBarActivity {
 
+    private static final int REQUEST_CONNECT_DEVICE = 1;
+    private static final int REQUEST_ENABLE_BT = 2;
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private BluetoothPrintDriver mChatService = null;
+
+    public static final int MESSAGE_STATE_CHANGE = 1;
+    public static final int MESSAGE_READ = 2;
+    public static final int MESSAGE_WRITE = 3;
+    public static final int MESSAGE_DEVICE_NAME = 4;
+    public static final int MESSAGE_TOAST = 5;
+
+
 
     public void onClick1(View view)
     {
-        Intent intent;
-        intent = new Intent(this,bluetooth.class);
-        startActivity(intent);
+        Toast.makeText(getApplicationContext(), "testpress", Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(setting.this, DeviceListActivity.class);
+        startActivityForResult(intent, REQUEST_CONNECT_DEVICE);
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // If BT is not on, request that it be enabled.
+        // setupChat() will then be called during onActivityResult
+        if (!mBluetoothAdapter.isEnabled()) {
+            Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+            // Otherwise, setup the chat session
+        } else {
+            if (mChatService == null) setupChat();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data)  {
+        switch (requestCode) {
+            case REQUEST_CONNECT_DEVICE:
+                // When DeviceListActivity returns with a device to connect
+                if (resultCode == Activity.RESULT_OK) {
+                    // Get the device MAC address
+                    String address = data.getExtras()
+                            .getString(DeviceListActivity.EXTRA_DEVICE_ADDRESS);
+                    // Get the BLuetoothDevice object
+                    Toast.makeText(getApplicationContext(),address , Toast.LENGTH_LONG).show();
+                    BluetoothDevice device = mBluetoothAdapter.getRemoteDevice(address);
+                    // Attempt to connect to the device
+                    mChatService.connect(device);
+
+                    BluetoothPrintDriver.Begin();
+
+                    String tmpContent = "Sunny!!";
+                    BluetoothPrintDriver.BT_Write(tmpContent);
+                    BluetoothPrintDriver.BT_Write("\r");
+
+
+                }
+                break;
+        }
+    }
+
+    private void setupChat() {
+        mChatService = new BluetoothPrintDriver(this, mHandler);
+    }
+
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_STATE_CHANGE:
+                    switch (msg.arg1) {
+                        case BluetoothPrintDriver.STATE_CONNECTED:
+                            break;
+                        case BluetoothPrintDriver.STATE_CONNECTING:
+                            break;
+                        case BluetoothPrintDriver.STATE_LISTEN:
+                        case BluetoothPrintDriver.STATE_NONE:
+                            break;
+                    }
+                    break;
+                case MESSAGE_WRITE:
+                    break;
+                case MESSAGE_READ:
+                    String ErrorMsg = null;
+                    byte[] readBuf = (byte[]) msg.obj;
+                    float Voltage = 0;
+                    if(readBuf[2]==0)
+                        ErrorMsg = "NO ERROR!         ";
+                    else
+                    {
+                        if((readBuf[2] & 0x02) != 0)
+                            ErrorMsg = "ERROR: No printer connected!";
+                        if((readBuf[2] & 0x04) != 0)
+                            ErrorMsg = "ERROR: No paper!  ";
+                        if((readBuf[2] & 0x08) != 0)
+                            ErrorMsg = "ERROR: Voltage is too low!  ";
+                        if((readBuf[2] & 0x40) != 0)
+                            ErrorMsg = "ERROR: Printer Over Heat!  ";
+                    }
+                    Voltage = (float) ((readBuf[0]*256 + readBuf[1])/10.0);
+                    //if(D) Log.i(TAG, "Voltage: "+Voltage);
+                    break;
+                case MESSAGE_DEVICE_NAME:
+                    break;
+                case MESSAGE_TOAST:
+                    break;
+            }
+        }
+    };
+
+
+    @Override
+    public synchronized void onResume() {
+        super.onResume();
+
+        if (mChatService != null) {
+            if (mChatService.getState() == BluetoothPrintDriver.STATE_NONE) {
+                // Start the Bluetooth chat services
+                mChatService.start();
+            }
+        }
+    }
+
+    @Override
+    public synchronized void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        // Stop the Bluetooth chat services
+        if (mChatService != null) mChatService.stop();
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     @Override
@@ -45,4 +193,10 @@ public class setting extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+
+
+
+
 }
